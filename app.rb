99c952299831ca616b7e26326440449ed320a140
jshields123 <jackshields@live.co.uk>
2021-01-19 17:09:23 +0000

    error

--- app.rb
+++ app.rb
@@ -6,5 +6,9 @@ class Battle < Sinatra::Base
     'Testing infrastructure working!'
   end
 
+  get '/' do
+    erb(:index)
+  end
+
   run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,17 @@
+
 feature 'Testing Infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
     expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
+
+feature 'enter names' do
+  scenario 'players to fill in names in a form ' do
+    visit('/')
+
+    fill_in :name, with: "Bob"
+    click_button "Create player"
+    expect(page).to have_content("Player Bob was created!")
   end
 end
