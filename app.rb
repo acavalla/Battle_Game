a7204cc97e028641a123597bf9c730c1a0260d98
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-01-21 14:46:30 +0000

    using sessions

--- app.rb
+++ app.rb
@@ -1,18 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
-    'Hello Battle!'
-    'Testing infrastructure working!'
-  end
 
-  get "/names" do
-    erb(:index)
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  get "/" do
+    erb :index
   end
 
   post "/names" do
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
+    session[:player1_name] = params[:player1_name]
+    session[:player2_name] = params[:player2_name]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
     erb :play
   end
 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,14 +1,7 @@
-
-feature 'Testing Infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
-
 feature 'enter names' do
+
   scenario 'Two players can fill in names in a form ' do
-    visit("/names")
+    visit("/")
 
     fill_in :player1_name, with: "Bob"
     fill_in :player2_name, with: "Bill"
