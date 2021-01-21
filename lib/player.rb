39b2c5ff087d9334a331a32c2ed818f322947749
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-01-21 17:59:08 +0000

    player.attack method works but hhp not displaying in attack.erb

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,17 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hp
 
-  def initialize(name)
+  def initialize(name, hp=100)
     @name = name
+    @hp = hp
   end
 
-end
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hp -= 10
+  end
+
+endquit
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -6,4 +6,10 @@ feature "attack" do
     expect(page).to have_content("Bill has been attacked! By Bob!")
   end
 
+  scenario "attacks reduce HP by 10" do
+    sign_in_and_play
+    click_button "Attack!"
+    expect(page).to have_content("Bill - Hit Points: 90")
+  end
+
 end
